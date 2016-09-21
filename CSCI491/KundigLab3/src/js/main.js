485517296dae76e4c4fa5c0583b202da72aeb914
Tobias Kundig <Kundigt@gmail.com>
2016-09-20 18:07:54 -0600

    Lab3 with working Bootstrap theme and picture modals

---
+++ CSCI491/KundigLab3/src/js/main.js
@@ -0,0 +1,39 @@
+function showLandscapeDiv() {
+
+    document.getElementById('Main').style.display="none";
+    document.getElementById('LandscapeDiv').style.display="block";
+    document.getElementById('WeddingsDiv').style.display="none";
+}
+
+function showWeddingsDiv() {
+
+    document.getElementById('Main').style.display="none";
+    document.getElementById('WeddingsDiv').style.display="block";
+    document.getElementById('LandscapeDiv').style.display="none";
+}
+
+function modalFunction(modalName){
+
+    // Get the modal
+    var modal = document.getElementById(modalName);
+
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById(modalName);
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+}
+
