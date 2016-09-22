e64a1118138837a462c84bb9159d1c851ea5101d
Tobias Kundig <Kundigt@gmail.com>
2016-09-22 08:58:09 -0600

    Cleaned up contactUs page with bootstrap elements, added js to validate, need to hookup.

--- CSCI491/HomeWork1/src/js/main.js
+++ CSCI491/HomeWork1/src/js/main.js
@@ -8,3 +8,22 @@ function showMainDiv(){
     document.getElementById('contactUs').style.display="none";
     document.getElementById('main').style.display="block";
 }
+ function showClintonFacts(){
+     document.getElementById('main').style.display="none";
+     document.getElementById('contactUs').style.display="none";
+     document.getElementById('clinton').style.display="block";
+ }
+function showTrumpFacts(){
+    document.getElementById('main').style.display="none";
+    document.getElementById('contactUs').style.display="none";
+    document.getElementById('clinton').style.display="none";
+    document.getElementById('trump').style.display="block";
+}
+
+function validateForm() {
+    var x = document.forms["form"]["name"].value;
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
