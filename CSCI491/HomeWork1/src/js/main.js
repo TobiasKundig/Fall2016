4e327eb7bb4007704cf65459d7ffaf0e51a4c323
Tobias Kundig <Kundigt@gmail.com>
2016-09-21 21:11:09 -0600

    Added pictures and contact us div with js to choose between

---
+++ CSCI491/HomeWork1/src/js/main.js
@@ -0,0 +1,10 @@
+function showContactUsDiv(){
+    document.getElementById('main').style.display="none";
+    document.getElementById('contactUs').style.display="block";
+
+}
+
+function showMainDiv(){
+    document.getElementById('contactUs').style.display="none";
+    document.getElementById('main').style.display="block";
+}
