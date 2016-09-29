55be5acf4304e7f2464abe65120543ca82e8c9ed
Tobias Kundig <Kundigt@gmail.com>
2016-09-29 16:02:53 -0600

    Moved the header to it's own page and linked it to all pages. Connected everything else.

--- CSCI491/HomeWork1/src/js/main.js
+++ CSCI491/HomeWork1/src/js/main.js
@@ -1,25 +1,3 @@
-function showContactUsDiv(){
-    document.getElementById('main').style.display="none";
-    document.getElementById('contactUs').style.display="block";
-
-}
-
-function showMainDiv(){
-    document.getElementById('contactUs').style.display="none";
-    document.getElementById('main').style.display="block";
-}
- function showClintonFacts(){
-     document.getElementById('main').style.display="none";
-     document.getElementById('contactUs').style.display="none";
-     document.getElementById('clinton').style.display="block";
- }
-function showTrumpFacts(){
-    document.getElementById('main').style.display="none";
-    document.getElementById('contactUs').style.display="none";
-    document.getElementById('clinton').style.display="none";
-    document.getElementById('trump').style.display="block";
-}
-
 function validateForm() {
     var x = document.forms["form"]["name"].value;
     if (x == null || x == "") {
